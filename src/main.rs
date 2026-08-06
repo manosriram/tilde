4ce7bf5180fd56f017e48fcd3c38ded29096ad67
Mano Sriram <mano.sriram0@gmail.com>
2026-08-07 00:12:58 +0530

    Make window interactive
    * Add support for keypresses and write to buffer on NamedKey::Enter

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ use std::sync::mpsc::{self, Sender};
 use std::{fs, thread};
 
 use fontdue::{Font, FontSettings, Metrics};
+use portable_pty::{CommandBuilder, PtyPair, PtySize, PtySystem, native_pty_system};
 use softbuffer::Surface;
+use std::sync::mpsc::channel;
 use winit::application::ApplicationHandler;
+use winit::event::ElementState::Pressed;
+use winit::event::WindowEvent::KeyboardInput;
 use winit::event::{self, WindowEvent};
 use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::keyboard::Key::Named;
+use winit::keyboard::PhysicalKey::{self, Code};
+use winit::keyboard::{Key, NamedKey};
 use winit::window::{Window, WindowAttributes, WindowId};
-use portable_pty::{CommandBuilder, PtyPair, PtySize, PtySystem, native_pty_system};
-use std::sync::mpsc::channel;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct Point {
@@ -32,13 +37,13 @@ struct App {
     grid: Vec<char>,
     channel_sender: Option<mpsc::Sender<Vec<u8>>>,
     channel_recv: Option<mpsc::Receiver<Vec<u8>>>,
-    cursor_position: Option<Point>
+    cursor_position: Option<Point>,
 }
 
 impl App {
     fn init(&mut self) {
         self.fontmap = HashMap::new();
-        self.cursor_position = Some(Point{ x:0, y:0 });
+        self.cursor_position = Some(Point { x: 0, y: 0 });
 
         self.grid = vec![' '; 6400];
 
@@ -46,14 +51,14 @@ impl App {
         self.channel_sender = Some(sender);
         self.channel_recv = Some(receiver);
 
-        let font = include_bytes!("/Users/manosriram/dev/tilde/src/assets/monospace-argon.otf") as &[u8];
+        let font =
+            include_bytes!("/Users/manosriram/dev/tilde/src/assets/monospace-argon.otf") as &[u8];
         let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
         for x in font.chars() {
             let f = font.rasterize(*x.0, 13.0);
             self.fontmap.insert(*x.0, f);
         }
         println!("{:?}", self.fontmap.get(&'a'));
-        
 
         let pty_system = native_pty_system();
         let pair = pty_system
@@ -80,7 +85,9 @@ impl App {
                         // print!("zz = {}", String::from_utf8_lossy(&buf[..n]));
                         tx.send(buf[..n].to_vec());
                     }
-                    Err(e) => {print!("{}", e)}
+                    Err(e) => {
+                        print!("{}", e)
+                    }
                 }
             }
         });
@@ -113,17 +120,12 @@ impl ApplicationHandler for App {
     }
 
     fn about_to_wait(&mut self, event_loop: &dyn ActiveEventLoop) {
-        println!("grid : {:?}", self.grid);
+        // println!("grid : {:?}", self.grid);
         let cm = self.channel_recv.as_ref().unwrap().try_recv();
         match cm {
             Ok(v) => {
                 let r = String::from_utf8(v);
                 for ch in r.unwrap().chars() {
-                    if self.cursor_position.as_mut().unwrap().x > 80 {
-                        self.cursor_position.as_mut().unwrap().y += 1;
-                        self.cursor_position.as_mut().unwrap().x = 0;
-                    }
-
                     if ch == '\n' {
                         self.cursor_position.as_mut().unwrap().y += 1;
                     }
@@ -131,72 +133,105 @@ impl ApplicationHandler for App {
                         self.cursor_position.as_mut().unwrap().x = 0;
                     }
 
-                    self.grid[self.cursor_position.unwrap().y * 80 + self.cursor_position.unwrap().x] = ch;
+                    if self.cursor_position.as_mut().unwrap().x > 80 {
+                        self.cursor_position.as_mut().unwrap().y += 1;
+                        self.cursor_position.as_mut().unwrap().x = 0;
+                    }
+
+                    self.grid
+                        [self.cursor_position.unwrap().y * 80 + self.cursor_position.unwrap().x] =
+                        ch;
                     self.cursor_position.as_mut().unwrap().x += 1;
                     self.window.as_ref().unwrap().request_redraw();
                 }
-            },
+            }
             Err(e) => {}
         };
 
         // println!("{:?}", cm);
     }
 
-    fn window_event(&mut self, event_loop: &dyn ActiveEventLoop, _id: WindowId, event: WindowEvent) {
-        if event == WindowEvent::CloseRequested {
-            event_loop.exit();
-        } else if event == WindowEvent::RedrawRequested {
-            println!("requesting after run");
-            println!("grid : {:?}", self.grid);
-
-            let size = self.window.as_ref().unwrap().surface_size();
-            let (width, height) = (size.width, size.height);
-            let Some(surface) = self.surface.as_mut() else {
-                return;
-            };
-            if width == 0 || height == 0 {
-                return;
+    fn window_event(
+        &mut self,
+        event_loop: &dyn ActiveEventLoop,
+        _id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
             }
-            surface
-                .resize(
-                    NonZeroU32::new(width).unwrap(),
-                    NonZeroU32::new(height).unwrap(),
-                )
-                .unwrap();
+            WindowEvent::RedrawRequested => {
+                println!("requesting after run");
+                println!("grid : {:?}", self.grid);
 
-            let mut buffer = surface.buffer_mut().unwrap();
+                let size = self.window.as_ref().unwrap().surface_size();
+                let (width, height) = (size.width, size.height);
+                let Some(surface) = self.surface.as_mut() else {
+                    return;
+                };
+                if width == 0 || height == 0 {
+                    return;
+                }
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
 
-            for y in 0..80 {
-                for x in 0..80 {
-                    let fmr = self.fontmap.get(&self.grid[(y * 80 + x) as usize]);
-                    match fmr {
-                        Some(fmi) => {
-                            let px = x * 13;
-                            let py = y * 13;
+                let mut buffer = surface.buffer_mut().unwrap();
 
-                            let (metrics, bitmap) = fmi;
-                            for gy in 0..metrics.height {
-                                for gx in 0..metrics.width {
-                                    let cvg = bitmap[gy * metrics.width + gx];
+                for y in 0..80 {
+                    for x in 0..80 {
+                        let fmr = self.fontmap.get(&self.grid[(y * 80 + x) as usize]);
+                        match fmr {
+                            Some(fmi) => {
+                                let px = x * 13;
+                                let py = y * 13;
 
-                                    let screen_x = px + metrics.xmin as usize + gx;
-                                    let screen_y = py + (13usize.saturating_sub(metrics.height)) + gy;
+                                let (metrics, bitmap) = fmi;
+                                for gy in 0..metrics.height {
+                                    for gx in 0..metrics.width {
+                                        let cvg = bitmap[gy * metrics.width + gx];
 
-                                    if screen_x < width as usize &&  screen_y < height as usize {
-                                        let c = cvg as u32;
-                                        let color = (c << 16) | (c << 8) | c;
-                                        buffer[screen_y * width as usize + screen_x] = color;
+                                        let screen_x = px + metrics.xmin as usize + gx;
+                                        let screen_y =
+                                            py + (13usize.saturating_sub(metrics.height)) + gy;
+
+                                        if screen_x < width as usize && screen_y < height as usize {
+                                            let c = cvg as u32;
+                                            let color = (c << 16) | (c << 8) | c;
+                                            buffer[screen_y * width as usize + screen_x] = 0;
+                                            buffer[screen_y * width as usize + screen_x] = color;
+                                        }
                                     }
                                 }
                             }
-                        },
-                        None => {}
+                            None => {}
+                        }
                     }
                 }
+                buffer.present().unwrap();
             }
-            buffer.present().unwrap();
-        } else {
-
+            WindowEvent::KeyboardInput { event, .. } => match event.state {
+                Pressed => match event.logical_key {
+                    Key::Named(k) => match k {
+                        NamedKey::Enter => {
+                            write!(self.writer.as_mut().unwrap(), "\n").unwrap();
+                        }
+                        _ => {}
+                    },
+                    Key::Character(ch) => {
+                        println!("{}", ch);
+                        write!(self.writer.as_mut().unwrap(), "{ch}").unwrap();
+                    }
+                    Key::Unidentified(_) => {}
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
         }
     }
 }
